bd30965b5168cffccda0fada37060decd7557498
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-27 01:30:53 -0700

    created navbar and functionality to open and close drawer for menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import { BiMenuAltRight } from "react-icons/bi";
+import { IoMdFitness } from "react-icons/io";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  console.log("Navbar: menuOpen: ", menuOpen);
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
+    // nav flex-row, space between
+    //menu hidden, needs state
     <div>
-      <h1 className="text-white max-w-xl">React</h1>
+      <IoMdFitness />
+      <h1 className="text-pink-400 text-center font-extralight max-w-xl">
+        Fitlab
+      </h1>
+      <BiMenuAltRight onClick={handleClick} />
+      {}
+      {menuOpen ? <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> : null}
     </div>
   );
 };
