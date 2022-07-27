0f65aab98003054ac3ceec7b32211c767f9ef9a8
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-27 01:45:05 -0700

    created jhero component and imported it to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import HeroComponent from "./components/HeroComponent";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroComponent />
     </div>
   );
 }
---
+++ src/components/HeroComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroComponent = () => {
+  return (
+    <div>
+      <img
+        src="https://visitncsmokies.com/wp-content/uploads/2018/09/HERO-PLACEHOLDER.jpg"
+        alt="hero"
+      />
+    </div>
+  );
+};
+
+export default HeroComponent;
