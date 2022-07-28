8151c3e1758fbad2246f53925ee96973da49e0f4
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-28 06:20:31 -0700

    implemented conditional rendering of the md size nav bar by detecting the window size and hiding the mobile menu when the right width was achieved

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import HeroComponent from "./components/HeroComponent";
 import Navbar from "./components/Navbar";
 import Exercises from "./components/Exercises";
@@ -6,9 +7,30 @@ import SponsorsBanner from "./components/SponsorsBanner";
 import CardContainer from "./components/CardsContainer";
 
 function App() {
+  const [detectSize, setDetectSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  console.log("detectSize: ", detectSize);
+
+  const getSize = () => {
+    setDetectSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", getSize);
+    return () => {
+      window.removeEventListener("resize", getSize);
+    };
+  }, [detectSize]);
+
   return (
     <div>
-      <Navbar />
+      <Navbar screenSize={detectSize} />
       <HeroComponent />
       <SponsorsBanner />
       <Exercises />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,60 @@ import Menu from "./Menu";
 import { BiMenuAltRight } from "react-icons/bi";
 import { IoMdFitness } from "react-icons/io";
 
-const Navbar = () => {
+const Navbar = (props) => {
+  console.log("Menu: props: ", props);
   const [menuOpen, setMenuOpen] = useState(false);
+  const screenSize = props.screenSize.width;
+  console.log("Navbar: screenSize: ", screenSize);
 
   const handleClick = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
-    // nav flex-row, space between
-    //menu hidden, needs state
-    <div className="flex justify-between items-center h-1/5 p-7 w-full bg-pink-50 ">
-      <IoMdFitness className="text-6xl px-2  text-pink-600" />
-      <h1 className="text-pink-600 text-center font-extralight max-w-xl text-5xl py-2 ">
-        Fitlab
-      </h1>
-      <BiMenuAltRight
-        className="text-6xl px-2 text-pink-600"
-        onClick={handleClick}
-      />
-      {menuOpen ? <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> : null}
-    </div>
+    <>
+      {screenSize < 768 ? (
+        <div className="flex justify-between items-center h-1/5 p-7 w-full bg-pink-50 ">
+          <IoMdFitness className="text-6xl px-2  text-pink-600" />
+          <h1 className="text-pink-600 text-center font-extralight max-w-xl text-5xl py-2 ">
+            Fitlab
+          </h1>
+          <BiMenuAltRight
+            className="text-6xl px-2 text-pink-600"
+            onClick={handleClick}
+          />
+          {menuOpen ? (
+            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          ) : null}
+        </div>
+      ) : (
+        <div className="flex justify-between align-middle h-1/5 p-6 w-full bg-pink-50 ">
+          <div className="flex flex-row ">
+            <IoMdFitness className="text-5xl px-2 py-2 text-pink-600" />
+            <h1 className="text-pink-600 text-center font-extralight max-w-xl text-4xl py-2 px-1 ">
+              Fitlab
+            </h1>
+          </div>
+          <div className="flex flex-row text-pink-600">
+            <ul className="flex">
+              <li className="p-3">Home</li>
+              <li className="p-3">
+                <a href="#exercises">Exercises</a>
+              </li>
+              <li className="p-3">
+                <a href="#trainers">Trainers</a>
+              </li>
+              <li className="p-3">About Us</li>
+            </ul>
+          </div>
+          <div className="flex">
+            <button className="flex px-3 py-4 bg-pink-600 text-white font-light rounded-xl">
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
