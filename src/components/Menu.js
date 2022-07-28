423ce11bdb8e937d721963e044f43cd4a92d0983
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-28 04:53:29 -0700

    styled menu for the mobile view

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,18 +12,20 @@ const Menu = (props) => {
   return (
     // make very wide in the vw
     // put the icon on the opposite side of the <li>s
-    <div className="z-30 absolute top-0 right-0 bg-stone-300 w-3/4 h-full">
-      <BiX onClick={handleClose} className="text-2xl" />
-      <ul>
-        <li>Home</li>
-        <li>
+    <div className="z-30 absolute top-0 right-0 bg-pink-600 text-white w-full h-full flex flex-col">
+      <div className="flex w-full justify-end">
+        <BiX onClick={handleClose} className="text-6xl flex" />
+      </div>
+      <ul className="w-full h-">
+        <li className="text-4xl p-3 text-center my-4">Home</li>
+        <li className="text-4xl p-3 text-center my-4">
           <a href="#exercises">Exercises</a>
         </li>
-        <li>
+        <li className="text-4xl p-3 text-center my-4">
           <a href="#trainers">Trainers</a>
         </li>
-        <li>About Us</li>
-        <li>Login</li>
+        <li className="text-4xl p-3 text-center my-4">About Us</li>
+        <li className="text-4xl p-3 text-center my-4">Login</li>
       </ul>
     </div>
   );
