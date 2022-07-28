5cab513d0068ca9cde221d73f0e93d7ad5b20510
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-28 06:22:34 -0700

    deleted console log

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { BiX } from "react-icons/bi";
 const Menu = (props) => {
   // give the component local state to handle the button to close
-
   const [menuState] = useState(props.menuOpen);
 
   const handleClose = () => {
@@ -10,13 +9,11 @@ const Menu = (props) => {
   };
 
   return (
-    // make very wide in the vw
-    // put the icon on the opposite side of the <li>s
-    <div className="z-30 absolute top-0 right-0 bg-pink-600 text-white w-full h-full flex flex-col">
-      <div className="flex w-full justify-end">
-        <BiX onClick={handleClose} className="text-6xl flex" />
+    <div className="z-30 absolute top-0 right-0 bg-pink-50 text-pink-600 w-full h-full flex flex-col">
+      <div onClick={handleClose} className="flex w-full justify-end">
+        <BiX className="text-6xl flex" />
       </div>
-      <ul className="w-full h-">
+      <ul onClick={handleClose} className="w-full h-">
         <li className="text-4xl p-3 text-center my-4">Home</li>
         <li className="text-4xl p-3 text-center my-4">
           <a href="#exercises">Exercises</a>
