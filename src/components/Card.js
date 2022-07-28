c4ae8ccad0e1b384b6bfe63c8ddaf147d669c2c5
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-28 03:08:59 -0700

    aligned cards in mobile view

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,12 @@ const Card = (props) => {
   console.log("Card: props.url: ", props);
   const { cardData } = props;
   return (
-    <div className="p-4">
-      <img className="w-full" src={cardData.url} alt="exercises" />
-      <h3>{cardData.header}</h3>
-      <p>{cardData.subtitle}</p>
+    <div className="mb-16 bg-pink-50 rounded-3xl pb-8 ">
+      <img className="w-full rounded-3xl" src={cardData.url} alt="exercises" />
+      <div className="p-1 ml-3">
+        <h3 className="text-4xl font-medium">{cardData.header}</h3>
+        <p className="text-pink-900 p-2.5 text-2xl">{cardData.subtitle}</p>
+      </div>
     </div>
   );
 };
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -3,8 +3,8 @@ import { data } from "../data";
 
 const CardContainer = () => {
   return (
-    <div className="flex-col min-w-full ">
-      <h2>Popular</h2>
+    <div className="flex-col min-w-full px-10 my-12 ">
+      <h2 className="text-6xl py-8 mb-6">Popular</h2>
       <div className="flex flex-col">
         {data.map((card, i) => (
           <Card cardData={card} key={i} />
