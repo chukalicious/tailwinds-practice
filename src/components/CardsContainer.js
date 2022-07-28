81bc48d063ead64682e620094a31517f6ab14c14
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-28 03:34:53 -0700

    added md screen alignment to the cardscontainer component

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -3,9 +3,9 @@ import { data } from "../data";
 
 const CardContainer = () => {
   return (
-    <div className="flex-col min-w-full px-10 my-12 ">
-      <h2 className="text-6xl py-8 mb-6">Popular</h2>
-      <div className="flex flex-col">
+    <div className="flex-col min-w-full px-10 my-12 mb:mx-12 ">
+      <h2 className="text-6xl py-8 mb-6 md:font-extralight">Popular</h2>
+      <div className="flex flex-col md:flex-row md flex-wrap min-w-full md:justify-evenly md:grid md:grid-cols-3 md:grid-flow-row md:gap-x-20">
         {data.map((card, i) => (
           <Card cardData={card} key={i} />
         ))}
