fd32a609dc30caf03e4b7724b3416aa50e29b7a3
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-28 00:06:37 -0700

    applied styles to align exercises section

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,23 +1,23 @@
 const Exercises = () => {
   return (
-    <div id="exercises" className="flex max-w-sm">
+    <div id="exercises" className="flex flex-col min-w-full md:flex-row">
       {" "}
-      <div className="flex flex-col">
-        <h3 className="text-3xl flex">Exercises</h3>
-        <p>
+      <div className="flex flex-col md:w-1/2 place-content-center">
+        <h3 className="flex mt-auto ml-20 text-3xl p-4 w-7/12 ">Exercises</h3>
+        <p className="p-4 my-7 ml-20 text-justify text-lg w-7/12">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Placerat
           orci nulla pellentesque dignissim. Felis bibendum ut tristique et
           egestas. Cras pulvinar mattis nunc sed blandit libero. Cursus mattis
           molestie a iaculis at erat pellentesque adipiscing.
         </p>
-        <button>Learn More</button>
+        <button className=" flex p-4 text-lg ml-20 w-fit mb-6">
+          Learn More
+        </button>
       </div>
-      <img
-        className="flex-row"
-        alt="random"
-        src="https://picsum.photos/400/400?random=2"
-      />{" "}
+      <div className="flex p-6 justify-center md:w-1/2">
+        <img alt="random" src="https://picsum.photos/600/400?random=2" />{" "}
+      </div>
     </div>
   );
 };
