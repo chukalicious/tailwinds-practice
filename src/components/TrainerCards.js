c6eb3bdcef579d7d3ec4c7abb151ac9760df493f
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-30 20:18:00 -0700

    applyed md screen styles

--- src/components/TrainerCards.js
+++ src/components/TrainerCards.js
@@ -1,7 +1,7 @@
 import { trainers } from "../data";
 const TrainerCards = () => {
   return (
-    <div>
+    <div className="pb-8">
       <h3 className="flex mt-8 ml-15 text-6xl p-4 max-w-fit md:ml-20 md:text-4xl md:font-thin">
         Meet Our Trainers
       </h3>
@@ -9,20 +9,22 @@ const TrainerCards = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </p>
-      {trainers.map((tr, i) => (
-        <div
-          key={i}
-          className="flex flex-col w-10/12 mb-8 mx-auto shadow-xl rounded-b-3xl "
-        >
-          <div className="flex flex-col">
-            <img className="rounded-t-3xl " src={tr.url} alt={tr.name} />{" "}
+      <div className="flex flex-col md:grid md:grid-cols-3 md:grid-flow-row px-16">
+        {trainers.map((tr, i) => (
+          <div
+            key={i}
+            className="flex flex-col w-10/12 mb-8 mx-auto shadow-xl rounded-b-3xl md:grid-cols-3"
+          >
+            <div className="flex flex-col">
+              <img className="rounded-t-3xl " src={tr.url} alt={tr.name} />{" "}
+            </div>
+            <div className="flex flex-col rounded-b-3xl text-center pb-10 bg-pink-50">
+              <h4>{tr.trainerName}</h4>
+              <p>Trainer</p>
+            </div>
           </div>
-          <div className="flex flex-col rounded-b-3xl text-center pb-10 bg-pink-50">
-            <h4>{tr.trainerName}</h4>
-            <p>Trainer</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
